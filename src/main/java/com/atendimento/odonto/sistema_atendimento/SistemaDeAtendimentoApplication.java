d533393574e5945c2231f966ca9c9a562adbe5b4
João Vítor Vieira Martins <joaovito9@MacBook-Air-de-Joao.local>
2025-05-27 19:41:45 -0300

    Commit inicial: Projeto atendimento.

---
+++ src/main/java/com/atendimento/odonto/sistema_atendimento/SistemaDeAtendimentoApplication.java
@@ -0,0 +1,13 @@
+package com.atendimento.odonto.sistema_atendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAtendimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAtendimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atendimento/odonto/sistema_atendimento/SistemaDeAtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atendimento.odonto.sistema_atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
